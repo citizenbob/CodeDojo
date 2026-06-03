5d47065b0ba8960b01d2e80b437ee12f8dba96c4
Robert Duebelbeis <citizenbob@users.noreply.github.com>
2026-06-02 21:14:02 -0400

    Adds comment highlighting code smell. Tests pass.

--- ParrotKata/CSharp/Parrot/Parrot.cs
+++ ParrotKata/CSharp/Parrot/Parrot.cs
@@ -18,6 +18,8 @@ namespace Parrot
             _isNailed = isNailed;
         }
 
+        // this method knows about three different _types
+        // of parrots and separate logic for each
         public double GetSpeed()
         {
             switch (_type)
