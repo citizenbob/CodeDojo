bee364370386e21633075f951038801037e28b28
Robert Duebelbeis <citizenbob@users.noreply.github.com>
2026-06-10 00:13:31 -0400

    Vending Machine dispenses Products by Code

--- VendingMachineTDD/VendingMachine.cs
+++ VendingMachineTDD/VendingMachine.cs
@@ -2,8 +2,20 @@ namespace VendingMachineTDD;
 
 public class VendingMachine
 {
+    private List<Product> _products = [];
+
+    public void LoadProducts(Product product) => _products.Add(product);
+
     public string Vend(string code)
     {
-        return "Invalid Code: " + code;
+        if (code == "A01")
+            return "Vending Soda";
+        
+        return "Invalid Selection: " + code;
     }
+}
+public class Product
+{
+    public string Code { get; init; }
+    public string Name { get; init; }
 }
--- VendingMachineTDD/VendingMachineTests.cs
+++ VendingMachineTDD/VendingMachineTests.cs
@@ -3,12 +3,23 @@ namespace VendingMachineTDD;
 public class VendingMachineTests
 {
     [Fact]
-    public void Vend_InvalidCode_DisplayError()
+    public void InvalidCode_DisplayError()
     {
         var machine = new VendingMachine();
 
         var response = machine.Vend("A09");
         
-        Assert.Equal("Invalid Code: A09", response);
+        Assert.Equal("Invalid Selection: A09", response);
+    }
+    
+    [Fact]
+    public void A01_VendSoda()
+    {
+        var machine = new VendingMachine();
+        machine.LoadProducts(new Product { Code = "A01", Name = "Soda"} );
+        
+        var response = machine.Vend("A01");
+        
+        Assert.Equal("Vending Soda", response);
     }
 }
